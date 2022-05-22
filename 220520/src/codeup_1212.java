f8375a061891663c276a24b558cbf903ca974f73
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-05-22 18:29:42 +0900

    코드업 1212번 풀이

--- 220520/src/codeup_1212.java
+++ 220520/src/codeup_1212.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
 
 public class codeup_1212 {
 	public static void main(String[] args) {
-		
+		Scanner in=new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		String result="no";
+		while(true) {
+			try {
+				System.out.println("삼각형의 성립 여부 분석을 시작합니다");
+				System.out.println("첫번째 변의 길이는 몇입니까?");
+				a=Integer.parseInt(in.nextLine());
+				System.out.println("두번째 변의 길이는 몇입니까?");
+				b=Integer.parseInt(in.nextLine());
+				System.out.println("세번째 변의 길이는 몇입니까?");
+				c=Integer.parseInt(in.nextLine());
+				if(a>b&&a>c) {
+					if(a<(b+c)) {
+						result="yes";
+					}
+				}
+				if(b>a&&b>c) {
+					if(b<(a+c)) {
+						result="yes";
+					}
+				}
+				if(c>b&&c>a) {
+					if(c<(a+b)) {
+						result="yes";
+					}
+				}
+				System.out.println("분석 결과 : "+result); break;
+			}
+			catch(NumberFormatException e) {
+				System.out.println("숫자를 입력하세요");
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 }
