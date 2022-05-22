f7669d5d11a33d2229ace896c76c60a64f5a9982
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-05-22 17:20:52 +0900

    Create backjun_2_7.java
    백준 2단계 7번 풀이

---
+++ 220520/src/backjun_2_7.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class backjun_2_7 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		while(true) {
+			int earn;
+			try {
+				System.out.println("첫번째 주사위를 입력하세요");
+				a=in.nextInt();
+				if(a<1||a>6) {
+					throw new Exception("오류 : 1~6사이의 숫자를 입력하세요");
+				}
+				System.out.println("두번째 주사위를 입력하세요");
+				b=in.nextInt();
+				if(b<1||b>6) {
+					throw new Exception("오류 : 1~6사이의 숫자를 입력하세요");
+				}
+				System.out.println("세번째 주사위를 입력하세요");
+				c=in.nextInt();
+				if(c<1||c>6) {
+					throw new Exception("오류 : 1~6사이의 숫자를 입력하세요");
+				}
+				if(a==b&&b==c) {
+					earn=10000+(a*1000);
+					System.out.println("상금은 "+earn+"원 입니다"); break;
+				}
+				if(a==b||a==c||b==c) {
+					earn=1000+(a*100);
+					System.out.println("상금은 "+earn+"원 입니다"); break;
+				}
+				if(a!=b&&b!=c) {
+					if(a>b&&a>c) {
+						earn=a*100;
+						System.out.println("상금은 "+earn+"원 입니다"); break;
+					}
+					if(b>c&&b>a) {
+						earn=b*100;
+						System.out.println("상금은 "+earn+"원 입니다"); break;
+					}
+					if(c>a&&c>b) {
+						earn=c*100;
+						System.out.println("상금은 "+earn+"원 입니다"); break;
+					}
+				}
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
